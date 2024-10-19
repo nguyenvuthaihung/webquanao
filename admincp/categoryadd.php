f6e442a9bf6a305352fda554096ac5e808b68d23
nguyenhungthai <thaihungchuyen@gmail.com>
2024-10-19 21:38:25 +0700

    them file

---
+++ admincp/categoryadd.php
@@ -0,0 +1,17 @@
+<?php
+include "header.php";
+include "slider.php"
+?>
+
+<div class="admin-content-right">
+            <div class="admin-content-right-category-add">
+                <h1>Thêm danh mục</h1>
+                <form action="" method="post">
+                    <input type="text" placeholder="Nhập tên danh mục">
+                    <button type="submit">Thêm</button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
